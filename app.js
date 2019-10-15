6ab98287efcbae954c642e76b68127aacf68d842
Yosof Badr <yosof.badr@siemens.com>
2019-10-15 14:50:27 +0100

    Add comments to explain the function of all the routes

--- app.js
+++ app.js
@@ -5,14 +5,8 @@ var mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true, useUnifiedTopology: true }); 
 
-// var campGrounds = [
-//   {name: "Hollow Moor", image: "https://images.unsplash.com/photo-1494545261862-dadfb7e1c13d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"},
-//   {name: "Ring Treat", image: "https://images.unsplash.com/photo-1482376292551-03dfcb8c0c74?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80"},
-//   {name: "Hollow Moor", image: "https://images.unsplash.com/photo-1494545261862-dadfb7e1c13d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"},
-//   {name: "Indian Ocean", image: "https://images.unsplash.com/photo-1455763916899-e8b50eca9967?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"},
-//   {name: "Ring Treat", image: "https://images.unsplash.com/photo-1482376292551-03dfcb8c0c74?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80"},
-//   {name: "Broke Mountain", image: "https://images.unsplash.com/photo-1500581276021-a4bbcd0050c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"},
-// ]
+//   {name: "Ring Treat", 
+// image: "https://images.unsplash.com/photo-1482376292551-03dfcb8c0c74?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80"},
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -25,10 +19,12 @@ var campgroundSchema = new mongoose.Schema({
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
+// Root Route - Display landing page for the application
 app.get("/", function(request, response) {
   response.render("landing")
 });
 
+// Index Route - Display all campgrounds
 app.get("/campgrounds", function(request, response) {
   Campground.find({}, function(err, allCampgrounds) {
     if (err)
@@ -38,6 +34,12 @@ app.get("/campgrounds", function(request, response) {
   }); 
 });
 
+// New Route - Display a form to create a new campground
+app.get("/campgrounds/new", function(request, response) {
+  response.render("newCamp")
+});
+
+// Create Route - Add a new campground to the DB
 app.post("/campgrounds", function(request, response) {
   var campName = request.body.campName;
   var campImage = request.body.campImage;
@@ -51,10 +53,6 @@ app.post("/campgrounds", function(request, response) {
       response.redirect("/campgrounds");
   });
 
-});
-
-app.get("/campgrounds/new", function(request, response) {
-  response.render("newCamp")
 });
 
 app.listen(3000, function() {
