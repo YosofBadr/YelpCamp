b55f17fb28b716308023d4f1de11c7c24a24e3ec
Yosof Badr <yosof.badr@siemens.com>
2019-10-15 14:19:37 +0100

    Add starter campground data to database

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/yelp_camp");
 
 var campGrounds = [
-  {name: "Indian Ocean", image: "https://images.unsplash.com/photo-1455763916899-e8b50eca9967?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"},
-  {name: "Broke Mountain", image: "https://images.unsplash.com/photo-1500581276021-a4bbcd0050c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"},
   {name: "Hollow Moor", image: "https://images.unsplash.com/photo-1494545261862-dadfb7e1c13d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"},
   {name: "Ring Treat", image: "https://images.unsplash.com/photo-1482376292551-03dfcb8c0c74?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80"},
   {name: "Hollow Moor", image: "https://images.unsplash.com/photo-1494545261862-dadfb7e1c13d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"},
@@ -26,6 +24,21 @@ var campgroundSchema = new mongoose.Schema({
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
+
+
+Campground.create(
+  {
+    name: "Indian Ocean",
+    image: "https://images.unsplash.com/photo-1455763916899-e8b50eca9967?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+  }
+);
+
+Campground.create(
+  {
+    name: "Broke Mountain",
+    image: "https://images.unsplash.com/photo-1500581276021-a4bbcd0050c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+  }
+);
 
 app.get("/", function(request, response) {
   response.render("landing")
