00bd12c85787cbaeeace6b1c1b139bb9aa58c0e3
Yosof Badr <yosof.badr@siemens.com>
2019-10-15 14:23:25 +0100

    Fixed some deprication warning caused by mongoose 'URL string parser is deprecated'

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true }); 
 
 var campGrounds = [
   {name: "Hollow Moor", image: "https://images.unsplash.com/photo-1494545261862-dadfb7e1c13d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"},
@@ -24,21 +24,6 @@ var campgroundSchema = new mongoose.Schema({
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
-
-
-Campground.create(
-  {
-    name: "Indian Ocean",
-    image: "https://images.unsplash.com/photo-1455763916899-e8b50eca9967?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-  }
-);
-
-Campground.create(
-  {
-    name: "Broke Mountain",
-    image: "https://images.unsplash.com/photo-1500581276021-a4bbcd0050c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-  }
-);
 
 app.get("/", function(request, response) {
   response.render("landing")
