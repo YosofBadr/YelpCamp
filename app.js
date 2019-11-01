338e15e67e1f8ac7be7077c55ce0c648c3908527
Yosof Badr <yosof_badr@hotmail.com>
2019-11-01 18:41:54 +0000

    Update styling of the show camp page

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true,
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 // Schemas
 var Campground = require("./models/campground");
