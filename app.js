4986fba225472fd9d3283a16a267eac6bb776265
Yosof Badr <yosof_badr@hotmail.com>
2019-11-01 18:13:01 +0000

    Add create route to save newly created comments

--- app.js
+++ app.js
@@ -78,6 +78,28 @@ app.get("/campgrounds/:id/comments/new", function(request, response) {
   });
 });
 
+
+// Create Route - Add a new comment and associate it to a campground in the DB
+app.post("/campgrounds/:id/comments", function(request, response){
+  Campground.findById(request.params.id, function(err, campground){
+    if(err) {
+      console.log("An error has occured: " + err);
+      response.redirect("/campgrounds");
+    }
+    else {
+      Comment.create(request.body.comment, function(err, comment){
+        if(err) 
+          console.log("An error has occured: " + err);   
+        else {
+          campground.comments.push(comment);
+          campground.save()
+          response.redirect("/campgrounds/" + campground._id);    
+        }
+      });
+    }
+  });
+});
+
 app.listen(3000, function() {
   console.log("YelpCamp listening on 3000");
 });
