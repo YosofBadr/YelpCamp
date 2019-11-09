e2b241ce9595f2e980a817b745e96d59c255135d
Yosof Badr <yosof_badr@hotmail.com>
2019-11-09 23:13:03 +0000

    Add connect flash to YelpCamp

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var methodOverride = require("method-override");
+var flash = require("connect-flash");
+
 
 
 // ================ AUTHENTICATION ======================
@@ -16,6 +18,7 @@ app.use(methodOverride("_method"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(flash());
 
 // Schemas
 var Campground = require("./models/campground");
